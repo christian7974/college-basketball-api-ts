0ee22a5b13003eb4cf9c03b96d3e51fd0589260c
christian7974 <ctropeano@uri.edu>
2024-10-07 13:12:17 -0400

    Added Compare Functionality
    User can query for two teams in the same endpoint.

--- src/routes/teamRoute.ts
+++ src/routes/teamRoute.ts
@@ -4,16 +4,26 @@ const router = express.Router();
 
 import {addAllTeams, showAllTeams, findTeamByName, sortTeams, getExtreme, compareTwoTeams, clearDatabase} from '../controllers/teamController';
 
+// Clear all of the teams in the database
 router.delete('/', clearDatabase);
 
+// Add all of the teams to the database
 router.post('/add-all', addAllTeams);
 
+// Query all of the teams in the country
 router.get('/all', showAllTeams);
 
+// Find a team by its name
 router.get('/:teamName', findTeamByName);
 
+// Sort the teams by a certain statistic
 router.get('/sort/:stat/:order', sortTeams);
 
+// Get the team with the most or the least of a statistic
 router.get('/extreme/:stat/:whichExtreme', getExtreme);
+
+// Compare two teams by their statistics
+router.get('/compare/:team1/:team2', compareTwoTeams);
+
 
 export default router;
